1f260327ab670b0e4e21c4e390fd60668a2ab823
alvaro rodrigues <alvim.lopez@gmail.com>
2023-10-26 20:15:38 -0300

    Criando repository e service para categoria

--- src/main/java/com/alvimcode/cursomc/CursomcApplication.java
+++ src/main/java/com/alvimcode/cursomc/CursomcApplication.java
@@ -2,8 +2,10 @@ package com.alvimcode.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.alvimcode.cursomc")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
