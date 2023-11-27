032932c577cec8b146c977907daeb10722a75a4e
alvaro rodrigues <alvim.lopez@gmail.com>
2023-11-27 15:51:27 -0300

    Implementação de novas classes

---
+++ src/main/java/com/alvimcode/cursomc/domain/Pedido.java
@@ -0,0 +1,80 @@
+package com.alvimcode.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Pedido implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private Date instante;
+	
+	private Pagamento pagamento;
+	
+	private Cliente cliente;
+	
+	private Endereco enderecoDeEntrega;
+	
+	public Pedido() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getInstante() {
+		return instante;
+	}
+
+	public void setInstante(Date instante) {
+		this.instante = instante;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Endereco getEnderecoDeEntrega() {
+		return enderecoDeEntrega;
+	}
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
